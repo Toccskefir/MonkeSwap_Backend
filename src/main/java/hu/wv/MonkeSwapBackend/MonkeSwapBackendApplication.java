61e85c0f1452d31e1ba7d760de508eb78c5f9e02
Tóth Kevin <tothkevin0716@gmail.com>
2024-02-13 13:53:19 +0100

    Initial commit

---
+++ src/main/java/hu/wv/MonkeSwapBackend/MonkeSwapBackendApplication.java
@@ -0,0 +1,13 @@
+package hu.wv.MonkeSwapBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkeSwapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonkeSwapBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/wv/MonkeSwapBackend/MonkeSwapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.wv.MonkeSwapBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonkeSwapBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
