609e9b0c6454d43fae3c07caf1f07fb125a03c0f
Tóth Kevin <tothkevin0716@gmail.com>
2024-02-15 10:07:11 +0100

    Created unique constraint on TradeOffer entity

--- src/main/java/hu/wv/MonkeSwapBackend/models/TradeOffer.java
+++ src/main/java/hu/wv/MonkeSwapBackend/models/TradeOffer.java
@@ -3,7 +3,10 @@ package hu.wv.MonkeSwapBackend.models;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "trade_offers")
+@Table(
+        name = "trade_offers",
+        uniqueConstraints = {@UniqueConstraint(name = "UK_ITEMS",columnNames = {"offered_item_id", "incoming_item_id"})}
+)
 public class TradeOffer {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
