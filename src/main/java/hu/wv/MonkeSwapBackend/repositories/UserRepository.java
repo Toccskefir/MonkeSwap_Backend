749f4b441798c69f549ec60c3ab130bbbc305c87
Tóth Kevin <tothkevin0716@gmail.com>
2024-02-21 11:43:31 +0100

    Added findByEmail to UserRepository

--- src/main/java/hu/wv/MonkeSwapBackend/repositories/UserRepository.java
+++ src/main/java/hu/wv/MonkeSwapBackend/repositories/UserRepository.java
@@ -4,7 +4,9 @@ import hu.wv.MonkeSwapBackend.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    Optional<User> findByEmail(String email);
 }
