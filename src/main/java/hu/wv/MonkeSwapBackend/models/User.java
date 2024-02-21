a8dd20ecff3e6bd41ddb0f0d4b3ed8c69dc0200c
Tóth Kevin <tothkevin0716@gmail.com>
2024-02-21 08:41:06 +0100

    Implemented UserDetails on User class

--- src/main/java/hu/wv/MonkeSwapBackend/models/User.java
+++ src/main/java/hu/wv/MonkeSwapBackend/models/User.java
@@ -4,14 +4,18 @@ import hu.wv.MonkeSwapBackend.enums.UserRole;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
 @Table(name = "users")
 @Data
-public class User {
+public class User implements UserDetails {
     //ATTRIBUTES
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -57,4 +61,29 @@ public class User {
 
     @OneToMany(mappedBy = "userId")
     private List<Notification> notifications;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
