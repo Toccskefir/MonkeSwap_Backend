58f996fab3e61aa085b3ebc1fca5edb7c2408a7f
Tóth Kevin <tothkevin0716@gmail.com>
2024-02-22 09:33:32 +0100

    Added AuthenticationService

---
+++ src/main/java/hu/wv/MonkeSwapBackend/services/AuthenticationService.java
@@ -0,0 +1,50 @@
+package hu.wv.MonkeSwapBackend.services;
+
+import hu.wv.MonkeSwapBackend.authentication.AuthenticationResponse;
+import hu.wv.MonkeSwapBackend.authentication.LoginRequest;
+import hu.wv.MonkeSwapBackend.authentication.RegisterRequest;
+import hu.wv.MonkeSwapBackend.enums.UserRole;
+import hu.wv.MonkeSwapBackend.models.User;
+import hu.wv.MonkeSwapBackend.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository repository;
+    private final PasswordEncoder encoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public void register(RegisterRequest request) {
+        User user = User.builder()
+                .username(request.getUsername())
+                .email(request.getEmail())
+                .password(encoder.encode(request.getPassword()))
+                .role(UserRole.USER)
+                .tradesCompleted(0)
+                .dateOfRegistration(LocalDate.now().toString())
+                .build();
+        repository.save(user);
+    }
+
+    public AuthenticationResponse login(LoginRequest request) {
+        authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                request.getEmail(),
+                request.getPassword()
+            )
+        );
+        User user = repository.findByEmail(request.getEmail()).orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
