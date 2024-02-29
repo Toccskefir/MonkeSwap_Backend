4d1221e1b1b5e1da599d81782d91c55dd7fd3c40
Tóth Kevin <tothkevin0716@gmail.com>
2024-02-29 09:50:02 +0100

    Added exceptions to AuthenticationService

--- src/main/java/hu/wv/MonkeSwapBackend/services/AuthenticationService.java
+++ src/main/java/hu/wv/MonkeSwapBackend/services/AuthenticationService.java
@@ -4,15 +4,19 @@ import hu.wv.MonkeSwapBackend.authentication.AuthenticationResponse;
 import hu.wv.MonkeSwapBackend.authentication.LoginRequest;
 import hu.wv.MonkeSwapBackend.authentication.RegisterRequest;
 import hu.wv.MonkeSwapBackend.enums.UserRole;
+import hu.wv.MonkeSwapBackend.exceptions.IsEmptyException;
+import hu.wv.MonkeSwapBackend.exceptions.IsRegisteredException;
 import hu.wv.MonkeSwapBackend.model.User;
 import hu.wv.MonkeSwapBackend.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.regex.Pattern;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +27,25 @@ public class AuthenticationService {
     private final AuthenticationManager authenticationManager;
 
     public void register(RegisterRequest request) {
+        if (repository.findByEmail(request.getEmail()).isPresent()) {
+            throw new IsRegisteredException("Email is already registered");
+        }
+        if (repository.findByUsername(request.getUsername()).isPresent()) {
+            throw new IsRegisteredException("Username is taken");
+        }
+        if (request.getEmail().isBlank()) {
+            throw new IsEmptyException("Email");
+        }
+        if (request.getUsername().isBlank()) {
+            throw new IsEmptyException("Username");
+        }
+        if (request.getPassword().isBlank()) {
+            throw new IsEmptyException("Password");
+        }
+        if (!Pattern.matches("^.+@.+\\..+$", request.getEmail())) {
+            throw new IllegalArgumentException("Email is in wrong format");
+        }
+
         User user = User.builder()
                 .username(request.getUsername())
                 .email(request.getEmail())
@@ -35,13 +58,21 @@ public class AuthenticationService {
     }
 
     public AuthenticationResponse login(LoginRequest request) {
+        if (request.getEmail().isBlank()) {
+            throw new IsEmptyException("Email");
+        }
+        if (request.getPassword().isBlank()) {
+            throw new IsEmptyException("Password");
+        }
+
         authenticationManager.authenticate(
             new UsernamePasswordAuthenticationToken(
                 request.getEmail(),
                 request.getPassword()
             )
         );
-        User user = repository.findByEmail(request.getEmail()).orElseThrow();
+
+        User user = repository.findByEmail(request.getEmail()).orElseThrow(() -> new BadCredentialsException("Wrong email or password"));
         var jwtToken = jwtService.generateToken(user);
         return AuthenticationResponse.builder()
                 .token(jwtToken)
