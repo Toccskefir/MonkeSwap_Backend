9bf3ad371a87ec597b1e6bfdec98b891c6b7dadc
Tóth Kevin <tothkevin0716@gmail.com>
2024-03-11 09:50:57 +0100

    Created /user/tradeoffers/offered and /user/tradeoffers/incoming endpoints

--- src/main/java/hu/wv/MonkeSwapBackend/controllers/UserController.java
+++ src/main/java/hu/wv/MonkeSwapBackend/controllers/UserController.java
@@ -1,8 +1,10 @@
 package hu.wv.MonkeSwapBackend.controllers;
 
 import hu.wv.MonkeSwapBackend.dtos.ItemDto;
+import hu.wv.MonkeSwapBackend.dtos.TradeOfferDto;
 import hu.wv.MonkeSwapBackend.dtos.UserDto;
 import hu.wv.MonkeSwapBackend.services.ItemService;
+import hu.wv.MonkeSwapBackend.services.TradeOfferService;
 import hu.wv.MonkeSwapBackend.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,11 +17,13 @@ import java.util.List;
 public class UserController {
     private final UserService userService;
     private final ItemService itemService;
+    private final TradeOfferService tradeOfferService;
 
     @Autowired
-    public UserController(UserService userService, ItemService itemService) {
+    public UserController(UserService userService, ItemService itemService, TradeOfferService tradeOfferService) {
         this.userService = userService;
         this.itemService = itemService;
+        this.tradeOfferService = tradeOfferService;
     }
     @GetMapping("/user")
     public UserDto getUser() {
@@ -34,6 +38,16 @@ public class UserController {
     @GetMapping("/user/items/{userId}")
     public List<ItemDto> getEnabledItemsByUserId(@PathVariable("userId")Long userId) {
         return this.itemService.getEnabledItemsByUserId(userId);
+    }
+
+    @GetMapping("/user/tradeoffers/offered")
+    public List<TradeOfferDto> getOfferedTradeOffers() {
+        return this.tradeOfferService.getOfferedTradeOffers();
+    }
+
+    @GetMapping("/user/tradeoffers/incoming")
+    public List<TradeOfferDto> getIncomingTradeOffers() {
+        return this.tradeOfferService.getIncomingTradeOffers();
     }
 
     @GetMapping("/admin/user/{userId}")
--- src/main/java/hu/wv/MonkeSwapBackend/services/TradeOfferService.java
+++ src/main/java/hu/wv/MonkeSwapBackend/services/TradeOfferService.java
@@ -1,18 +1,60 @@
 package hu.wv.MonkeSwapBackend.services;
 
+import hu.wv.MonkeSwapBackend.dtos.TradeOfferDto;
+import hu.wv.MonkeSwapBackend.enums.ItemState;
+import hu.wv.MonkeSwapBackend.model.Item;
 import hu.wv.MonkeSwapBackend.model.TradeOffer;
+import hu.wv.MonkeSwapBackend.repositories.ItemRepository;
 import hu.wv.MonkeSwapBackend.repositories.TradeOfferRepository;
+import hu.wv.MonkeSwapBackend.utils.CommonUtil;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class TradeOfferService {
     private final TradeOfferRepository tradeOfferRepository;
+    private final ItemRepository itemRepository;
 
     @Autowired
-    public TradeOfferService(TradeOfferRepository tradeOfferRepository) {
+    public TradeOfferService(TradeOfferRepository tradeOfferRepository, ItemRepository itemRepository) {
         this.tradeOfferRepository = tradeOfferRepository;
+        this.itemRepository = itemRepository;
+    }
+
+    private TradeOfferDto convertTradeOfferToTradeOfferDto(TradeOffer item) {
+        return TradeOfferDto.builder()
+                .id(item.getId())
+                .offeredItem(item.getOfferedItem())
+                .incomingItem(item.getIncomingItem())
+                .comment(item.getComment())
+                .build();
+    }
+
+    public List<TradeOfferDto> getOfferedTradeOffers() {
+        List<Item> items = itemRepository
+                .findAllByUserIdAndState(CommonUtil.getUserFromContextHolder(), ItemState.ENABLED);
+        List<TradeOfferDto> offeredTradeOffers = new ArrayList<>();
+        items.forEach(item -> {
+            Optional<TradeOffer> tradeOffer = this.tradeOfferRepository.findTradeOfferByOfferedItem(item);
+            tradeOffer.ifPresent(offer -> offeredTradeOffers.add(this.convertTradeOfferToTradeOfferDto(offer)));
+        });
+        return offeredTradeOffers;
+    }
+
+    public List<TradeOfferDto> getIncomingTradeOffers() {
+        List<Item> items = itemRepository
+                .findAllByUserIdAndState(CommonUtil.getUserFromContextHolder(), ItemState.ENABLED);
+        List<TradeOfferDto> incomingTradeOffers = new ArrayList<>();
+        items.forEach(item -> {
+            Optional<TradeOffer> tradeOffer = this.tradeOfferRepository.findTradeOfferByIncomingItem(item);
+            tradeOffer.ifPresent(offer -> incomingTradeOffers.add(this.convertTradeOfferToTradeOfferDto(offer)));
+        });
+        return incomingTradeOffers;
     }
 
     @Transactional
