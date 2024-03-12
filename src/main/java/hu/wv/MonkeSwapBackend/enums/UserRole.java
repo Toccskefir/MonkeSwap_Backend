70c7eaa19b27c628eb4b5ba20121eb6a98956af7
Tóth Kevin <tothkevin0716@gmail.com>
2024-03-12 10:03:49 +0100

    Added findByName method to UserRole enum class

--- src/main/java/hu/wv/MonkeSwapBackend/enums/UserRole.java
+++ src/main/java/hu/wv/MonkeSwapBackend/enums/UserRole.java
@@ -3,4 +3,15 @@ package hu.wv.MonkeSwapBackend.enums;
 public enum UserRole {
     USER,
     ADMIN;
+
+    public static UserRole findByName(String name) {
+        UserRole result = null;
+        for (UserRole role : values()) {
+            if (role.name().equalsIgnoreCase(name)) {
+                result = role;
+                break;
+            }
+        }
+        return result;
+    }
 }
