034d1752d9741f2fd14e9d19919865f5a98d908c
Tóth Kevin <tothkevin0716@gmail.com>
2024-03-18 09:58:11 +0100

    Added findByName method to NotificationType enum class

--- src/main/java/hu/wv/MonkeSwapBackend/enums/NotificationType.java
+++ src/main/java/hu/wv/MonkeSwapBackend/enums/NotificationType.java
@@ -3,4 +3,15 @@ package hu.wv.MonkeSwapBackend.enums;
 public enum NotificationType {
     NOTIFICATION,
     WARNING;
+
+    public static NotificationType findByName(String name) {
+        NotificationType result = null;
+        for (NotificationType state : values()) {
+            if (state.name().equalsIgnoreCase(name)) {
+                result = state;
+                break;
+            }
+        }
+        return result;
+    }
 }
