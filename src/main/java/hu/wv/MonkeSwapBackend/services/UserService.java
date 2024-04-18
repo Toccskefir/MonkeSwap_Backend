dc14b58b571c21308539353afe186a5bb21f6f1d
Tóth Kevin <tothkevin0716@gmail.com>
2024-04-18 10:17:23 +0200

    Added validation for full name and phone number of user update service

--- src/main/java/hu/wv/MonkeSwapBackend/services/UserService.java
+++ src/main/java/hu/wv/MonkeSwapBackend/services/UserService.java
@@ -68,6 +68,12 @@ public class UserService {
         if (!Pattern.matches("^[a-zA-Z\\d]+$", userDto.getUsername())) {
             throw new IllegalArgumentException("Username can't contain any special character");
         }
+        if (userDto.getFullName() != null &&!Pattern.matches("^[a-z A-Z]+$", userDto.getFullName())) {
+            throw new IllegalArgumentException("Full name can't contain any special character or number");
+        }
+        if (userDto.getPhoneNumber() != null && !Pattern.matches("^[+][\\d]+$", userDto.getPhoneNumber())) {
+            throw new IllegalArgumentException("Phone number should start with a + sign and contain only numbers");
+        }
 
         loggedInUser.setUsername(userDto.getUsername());
         loggedInUser.setFullName(userDto.getFullName());
